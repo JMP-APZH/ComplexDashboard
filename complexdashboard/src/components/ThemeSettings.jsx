c9fa64e07b86128c3df463b318362d340a893894
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-06-22 11:18:11 +0200

    LineChart setup completed

--- complexdashboard/src/components/ThemeSettings.jsx
+++ complexdashboard/src/components/ThemeSettings.jsx
@@ -16,7 +16,7 @@ const ThemeSettings = () => {
         // isOpen={themeSettings}
         className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
         <div 
-          className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+          className='float-right h-screen dark:text-gray-900 bg-white dark:[#484B52] w-400'>
           
           <div 
             className='flex justify-between items-center p-4 ml-4'
